70a5718fb6e528593e99897346b611a15ac9acd9
aniaska4 <aniaska4@wp.pl>
2019-04-09 20:36:26 +0200

    change price function and some style in price_main

--- app.js
+++ app.js
@@ -23,46 +23,39 @@ document.addEventListener('DOMContentLoaded', function () {
                         image.src = this.getAttribute('src');
                     }
                 }
-
             };
+
+            //price details
+
+            const priceAll = function (a, b) {
+                a.addEventListener('click', () => {
+                    if (b.style.display == "none") {
+                        b.style.display = "block"
+                    } else {
+                        b.style.display = "none"
+                    }
+                });
+            }
 
             //price hand
 
             const price = document.querySelector(".price_hand");
             const details = document.querySelector('.price_hand_main');
-            price.addEventListener('click', () => {
-                if (details.style.display === "none") {
-                    details.style.display = "block"
-                } else {
-                    details.style.display = "none"
-                }
-            });
+            priceAll(price, details);
 
             //price feet
 
             const priceFeet = document.querySelector(".price_feet");
             const detailsFeet = document.querySelector('.price_feet_main');
-            priceFeet.addEventListener('click', () => {
-                if (detailsFeet.style.display === "none") {
-                    detailsFeet.style.display = "block"
-                } else {
-                    detailsFeet.style.display = "none"
-                }
-            });
+            priceAll(priceFeet, detailsFeet);           
 
 
             //price cosmetics
 
             const priceCosmetics = document.querySelector(".price_cosmetics");
             const detailsCosmetics = document.querySelector('.price_cosmetics_main');
+            priceAll(priceCosmetics, detailsCosmetics);
 
-            priceCosmetics.addEventListener('click', () => {
-                if (detailsCosmetics.style.display === "none") {
-                    detailsCosmetics.style.display = "block"
-                } else {
-                    detailsCosmetics.style.display = "none"
-                }
-            });
 
 
             //up button
